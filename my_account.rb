a9ce3dda9101f6dc099481a809ad6ac4b89ea31f
Monique Rio <mrio@umich.edu>
2020-10-22 09:31:45 -0400

    added scaffolding

--- my_account.rb
+++ my_account.rb
@@ -7,17 +7,26 @@ require_relative "./models/patron"
 require_relative "./models/alma_client"
 require_relative "./models/alma_error"
 
+require_relative "./models/prototypes/loans"
+
 enable :sessions
 
 get '/' do
-  session[:uniqname] = 'mrio' #need to get this from cosign?
-  "Hello World"
+  loans = Prototypes::Loans.new
+  erb :shelf, :locals => {loans: loans} 
 end
 
+get '/notifications' do
+  erb :notifications
+end
+
+get '/fines' do
+  erb :fines
+end
+
+#demos info from Alma
 get '/profile' do 
   session[:uniqname] = 'mrio' #need to get this from cosign?
   patron = Patron.for(uniqname: session[:uniqname])
   erb :patron, :locals => {patron: patron}
 end
-  
-
