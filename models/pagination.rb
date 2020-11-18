f8a875cea8bd24ad37b18d6ea857c0f75898cac2
Monique Rio <mrio@umich.edu>
2020-11-18 14:13:03 -0500

    pagination calculation working

--- models/pagination.rb
+++ models/pagination.rb
@@ -1,39 +1,50 @@
 class Pagination
-  def initialize(url:, current_offset:, total:, limit: )
-    @url = url
+  def initialize(current_offset:, total:, limit: )
     @current_offset = current_offset.to_i
     @total = total.to_i
     @limit = limit.to_i
     @max_pages = 5
   end
-  def first
-    @current_offset + 1 
+  def previous
+    prev_offset = @current_offset - @limit
+    prev_offset = 0 if prev_offset < 0
+    is_current_page = (prev_offset == @current_offset) 
+    page(prev_offset, is_current_page)
   end
-  def last
-    @current_offset + @limit
-  end
-  def previous_offset
-    @current_offset - @limit
-  end
-  def next_offset
-    @current_offset + @limit
+  def next
+    next_offset = @current_offset + @limit
+    next_offset = @current_offset if next_offset >= @total
+    is_current_page = (next_offset == @current_offset)
+    page(next_offset, is_current_page)
   end
 
   def pages
     array = []
     middle = (@max_pages / 2) + 1
-    if @current_offset < (middle - 1) * @limit #for pages less than the middle
+    outside = (middle - 1) * @limit
+    if @current_offset < outside #for pages less than the middle
       (1 .. @max_pages).each do |i|
         offset = (i -1) * @limit
         is_current_page = (@current_offset == offset)
-        array.push(Page.new(offset: offset, is_current_page: is_current_page, limit: @limit)) if offset < @total
+        array.push(page(offset, is_current_page)) if offset < @total
       end
-    else #for pages greater than the middle
+    elsif @current_offset < @total - outside #for pages greater than the middle
       (1 .. @max_pages).each do |i| 
         addend = (i-middle) * @limit
         offset = @current_offset + addend
         is_current_page = (@current_offset == offset)
-        array.push(Page.new(offset: offset, is_current_page: is_current_page, limit: @limit)) if offset < @total
+        array.push(page(offset, is_current_page)) if offset < @total
+      end
+    else
+      diff = @total - @current_offset
+      curr_offset_pos_from_end = @max_pages - (diff / @limit)
+      curr_offset_pos_from_end = curr_offset_pos_from_end + 1 if diff % @limit == 0
+      
+      (1 .. @max_pages).each do |i|
+        addend = (i-curr_offset_pos_from_end)*@limit
+        offset = @current_offset + addend
+        is_current_page = (@current_offset == offset)
+        array.push(page(offset, is_current_page))
       end
     end
     array
@@ -53,20 +64,23 @@ class Pagination
 
   private_constant :Page
   private
+  def page(offset, is_current_page)
+    Page.new(offset: offset, is_current_page: is_current_page, limit: @limit)
+  end
   #def max_pages
     #base_pages = @total / @limit
     #@total % @limit > 0 ?  extra_page = 1 : extra_page = 0
     #base_pages + mod_page
   #end
-  def path(offset)
-    query = []
-    query.push("offset=#{offset}") if offset > 0
-    query.push("limit=#{@limit}") if @limit != 10
-    query_string = query.join('&')
-    query_string = nil if query_string == ''
+  #def path(offset)
+    #query = []
+    #query.push("offset=#{offset}") if offset > 0
+    #query.push("limit=#{@limit}") if @limit != 10
+    #query_string = query.join('&')
+    #query_string = nil if query_string == ''
 
-    URI::HTTP.build(path: @url, query: query_string).request_uri
-  end
+    #URI::HTTP.build(path: @url, query: query_string).request_uri
+#  end
 
   
   
--- spec/models/pagination_spec.rb
+++ spec/models/pagination_spec.rb
@@ -2,28 +2,77 @@ require 'spec_helper'
 require 'json'
 
 describe Pagination do
+  context "many results; first page; default limit" do
+    subject do
+      Pagination.new(current_offset: 0, limit: 10, total: 100)
+    end
+    context "#previous" do
+      it "returns previous page offset" do
+        expect(subject.previous.offset).to eq(0)
+      end
+      it "is current page" do
+        expect(subject.previous.current_page?).to eq(true)
+      end
+    end
+    context "#next" do
+      it "returns next page offset" do
+        expect(subject.next.offset).to eq(10)
+      end
+      it "is not current page" do
+        expect(subject.next.current_page?).to eq(false)
+      end
+    end
+    context "#pages" do
+      let(:pages) { subject.pages }
+      it "returns an array of 5 Pages" do
+        expect(pages.count).to eq(5)
+        expect(pages[0].class.name).to eq('Pagination::Page')
+      end
+      
+      it "returns first Page" do
+        page = pages[0]
+        expect(page.offset).to eq(0)
+        expect(page.current_page?).to be_truthy
+        expect(page.page_number).to eq(1)
+      end
+      it "returns second Page" do
+        page = pages[1]
+        expect(page.offset).to eq(10)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(2)
+      end
+      it "returns third Page" do
+        page = pages[2]
+        expect(page.offset).to eq(20)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(3)
+      end
+      it "returns fourth Page" do
+        page = pages[3]
+        expect(page.offset).to eq(30)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(4)
+      end
+      it "returns fifth Page" do
+        page = pages[4]
+        expect(page.offset).to eq(40)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(5)
+      end
+    end
+  end
   context "many results; one page over; default limit" do
     subject do
-      Pagination.new(url: '/things', current_offset: 10, limit: 10, total: 100)
+      Pagination.new(current_offset: 10, limit: 10, total: 100)
     end
-    context "#previous_offset" do
+    context "#previous" do
       it "returns previous page offset" do
-        expect(subject.previous_offset).to eq(0)
+        expect(subject.previous.offset).to eq(0)
       end
     end
-    context "#next_offset" do
+    context "#next" do
       it "returns next page offset" do
-        expect(subject.next_offset).to eq(20)
-      end
-    end
-    context "first" do
-      it "returns integer of item index of first shown item" do
-        expect(subject.first).to eq(11)
-      end
-    end
-    context "#last" do
-      it "returns integer of item index of last shown item" do
-        expect(subject.last).to eq(20)
+        expect(subject.next.offset).to eq(20)
       end
     end
     context "#pages" do
@@ -57,11 +106,248 @@ describe Pagination do
         expect(page.current_page?).to be_falsey
         expect(page.page_number).to eq(4)
       end
-      it "returns fith Page" do
+      it "returns fifth Page" do
         page = pages[4]
         expect(page.offset).to eq(40)
         expect(page.current_page?).to be_falsey
         expect(page.page_number).to eq(5)
+      end
+    end
+  end
+  context "many results; middle of set; default limit" do
+    subject do
+      Pagination.new(current_offset: 50, limit: 10, total: 100)
+    end
+    context "#previous" do
+      it "returns previous page offset" do
+        expect(subject.previous.offset).to eq(40)
+      end
+      it "is not current page" do
+        expect(subject.previous.current_page?).to eq(false)
+      end
+    end
+    context "#next" do
+      it "returns next page offset" do
+        expect(subject.next.offset).to eq(60)
+      end
+      it "is not current page" do
+        expect(subject.next.current_page?).to eq(false)
+      end
+    end
+    context "#pages" do
+      let(:pages) { subject.pages }
+      
+      it "returns first Page" do
+        page = pages[0]
+        expect(page.offset).to eq(30)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(4)
+      end
+      it "returns second Page" do
+        page = pages[1]
+        expect(page.offset).to eq(40)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(5)
+      end
+      it "returns third Page" do
+        page = pages[2]
+        expect(page.offset).to eq(50)
+        expect(page.current_page?).to be_truthy
+        expect(page.page_number).to eq(6)
+      end
+      it "returns fourth Page" do
+        page = pages[3]
+        expect(page.offset).to eq(60)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(7)
+      end
+      it "returns fifth Page" do
+        page = pages[4]
+        expect(page.offset).to eq(70)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(8)
+      end
+    end
+  end
+  context "many results; next to last page of set; default limit" do
+    subject do
+      Pagination.new(current_offset: 80, limit: 10, total: 100)
+    end
+    context "#previous" do
+      it "returns previous page offset" do
+        expect(subject.previous.offset).to eq(70)
+      end
+      it "is not current page" do
+        expect(subject.previous.current_page?).to eq(false)
+      end
+    end
+    context "#next" do
+      it "returns next page offset" do
+        expect(subject.next.offset).to eq(90)
+      end
+      it "is not current page" do
+        expect(subject.next.current_page?).to eq(false)
+      end
+    end
+    context "#pages" do
+      let(:pages) { subject.pages }
+      
+      it "returns first Page" do
+        page = pages[0]
+        expect(page.offset).to eq(50)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(6)
+      end
+      it "returns second Page" do
+        page = pages[1]
+        expect(page.offset).to eq(60)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(7)
+      end
+      it "returns third Page" do
+        page = pages[2]
+        expect(page.offset).to eq(70)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(8)
+      end
+      it "returns fourth Page" do
+        page = pages[3]
+        expect(page.offset).to eq(80)
+        expect(page.current_page?).to be_truthy
+        expect(page.page_number).to eq(9)
+      end
+      it "returns fifth Page" do
+        page = pages[4]
+        expect(page.offset).to eq(90)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(10)
+      end
+    end
+  end
+  context "many results; last page of set; default limit" do
+    subject do
+      Pagination.new(current_offset: 90, limit: 10, total: 100)
+    end
+    context "#previous" do
+      it "returns previous page offset" do
+        expect(subject.previous.offset).to eq(80)
+      end
+      it "is not current page" do
+        expect(subject.previous.current_page?).to eq(false)
+      end
+    end
+    context "#next" do
+      it "returns next page offset" do
+        expect(subject.next.offset).to eq(90)
+      end
+      it "is not current page" do
+        expect(subject.next.current_page?).to eq(true)
+      end
+    end
+    context "#pages" do
+      let(:pages) { subject.pages }
+      
+      it "returns first Page" do
+        page = pages[0]
+        expect(page.offset).to eq(50)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(6)
+      end
+      it "returns second Page" do
+        page = pages[1]
+        expect(page.offset).to eq(60)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(7)
+      end
+      it "returns third Page" do
+        page = pages[2]
+        expect(page.offset).to eq(70)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(8)
+      end
+      it "returns fourth Page" do
+        page = pages[3]
+        expect(page.offset).to eq(80)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(9)
+      end
+      it "returns fifth Page" do
+        page = pages[4]
+        expect(page.offset).to eq(90)
+        expect(page.current_page?).to be_truthy
+        expect(page.page_number).to eq(10)
+      end
+    end
+  end
+  context "two pages of results; first page; default limit" do
+    subject do
+      Pagination.new(current_offset: 0, limit: 10, total: 12)
+    end
+    context "#previous" do
+      it "returns previous page offset" do
+        expect(subject.previous.offset).to eq(0)
+      end
+      it "is not current page" do
+        expect(subject.previous.current_page?).to eq(true)
+      end
+    end
+    context "#next" do
+      it "returns next page offset" do
+        expect(subject.next.offset).to eq(10)
+      end
+      it "is not current page" do
+        expect(subject.next.current_page?).to eq(false)
+      end
+    end
+    context "#pages" do
+      let(:pages) { subject.pages }
+      it "returns array with 2 elements" do
+        expect(pages.count).to eq(2)
+      end
+      it "returns first Page" do
+        page = pages[0]
+        expect(page.offset).to eq(0)
+        expect(page.current_page?).to be_truthy
+        expect(page.page_number).to eq(1)
+      end
+      it "returns second Page" do
+        page = pages[1]
+        expect(page.offset).to eq(10)
+        expect(page.current_page?).to be_falsey
+        expect(page.page_number).to eq(2)
+      end
+    end
+  end
+  context "one page of results; default limit" do
+    subject do
+      Pagination.new(current_offset: 0, limit: 10, total: 6)
+    end
+    context "#previous" do
+      it "returns previous page offset" do
+        expect(subject.previous.offset).to eq(0)
+      end
+      it "is current page" do
+        expect(subject.previous.current_page?).to eq(true)
+      end
+    end
+    context "#next" do
+      it "returns next page offset" do
+        expect(subject.next.offset).to eq(0)
+      end
+      it "is current page" do
+        expect(subject.next.current_page?).to eq(true)
+      end
+    end
+    context "#pages" do
+      let(:pages) { subject.pages }
+      it "returns array with 2 elements" do
+        expect(pages.count).to eq(1)
+      end
+      it "returns first Page" do
+        page = pages[0]
+        expect(page.offset).to eq(0)
+        expect(page.current_page?).to be_truthy
+        expect(page.page_number).to eq(1)
       end
     end
   end
