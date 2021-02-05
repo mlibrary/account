eb73aad8ae5fd6bdedcc6e243b0861f7e53e5ab2
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-02-05 13:38:59 -0500

    Removing `get_all` from Client.

--- models/illiad_client.rb
+++ models/illiad_client.rb
@@ -12,25 +12,4 @@ class ILLiadClient
   def get(url, query={})
     self.class.get(url, query: query)
   end
-
-  def get_all(url:, record_key:, limit: 100, query: {})
-    query[:offset] = 0 
-    query[:limit] = limit
-    output = get(url, query)
-    if output.code == 200
-      body = output.parsed_response
-      while  body['total_record_count'] > limit + query[:offset]
-        query[:offset] = query[:offset] + limit
-        my_output = get(url, query) 
-        if my_output.code == 200
-          my_output.parsed_response[record_key].each {|x| body[record_key].push(x)}
-        else
-          return my_output #return error
-        end
-      end 
-      ::Response.new(parsed_response: body) #return good response
-    else
-      output #return error
-    end
-  end 
 end
