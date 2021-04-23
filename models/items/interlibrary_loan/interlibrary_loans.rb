2dc909a96e47a4ebaa78be9b7e28c3afaaf2365f
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-04-23 10:52:30 -0400

    Adding model/view for ILLiad checkouts

---
+++ models/items/interlibrary_loan/interlibrary_loans.rb
@@ -0,0 +1,19 @@
+class InterlibraryLoans < Items
+  def initialize(parsed_response:)
+    super
+    @items = parsed_response.filter_map { |item| InterlibraryLoan.new(item) if item["RequestType"] != "Loan" && item["TransactionStatus"] != "Request Finished" }
+  end
+
+  def self.for(uniqname:, client: ILLiadClient.new)
+    url = "/Transaction/UserRequests/#{uniqname}" 
+    response = client.get(url)
+    if response.code == 200
+      InterlibraryLoans.new(parsed_response: response.parsed_response)
+    else
+      #Error!
+    end
+  end
+end
+
+class InterlibraryLoan < InterlibraryLoanItem
+end
