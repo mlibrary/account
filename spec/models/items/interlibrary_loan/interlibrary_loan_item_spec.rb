e474e22dd76aa3bd27d0ff6147795c93accc55dd
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-05-04 14:02:13 -0400

    Fixing/adding ILLiad tests

---
+++ spec/models/items/interlibrary_loan/interlibrary_loan_item_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'json'
+
+describe InterlibraryLoanItem do
+  before(:each) do
+    @item = JSON.parse(File.read("./spec/fixtures/illiad_requests.json"))[2]
+  end
+  subject do
+    InterlibraryLoanItem.new(@item) 
+  end
+  context "#title" do
+    it "returns title string" do
+      expect(subject.title).to eq("Journal of Stuff and Things")
+    end
+  end
+  context "#author" do
+    it "returns author string" do
+      expect(subject.author).to eq("A. Greta Mind")
+    end
+  end
+  context "#illiad_url" do
+    it "returns url to the ILLiad transaction" do
+      expect(subject.illiad_url).to eq("https://ill.lib.umich.edu/illiad/illiad.dll?Action=10&Form=72&Value=3298020")
+    end
+  end
+  context "#creation_date" do
+    it "returns creation date string" do
+      expect(subject.creation_date).to eq("Mar 9, 2021")
+    end
+  end
+  context "#expiration_date" do
+    it "returns expiration date string" do
+      expect(subject.expiration_date).to eq('')
+    end
+  end
+end
--- spec/models/items/interlibrary_loan/interlibrary_loans_spec.rb
+++ spec/models/items/interlibrary_loan/interlibrary_loans_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 require 'json'
 
-describe InterlibraryLoanRequests do
-  context "zero loans" do
+describe InterlibraryLoans do
+  context "one loan" do
     before(:each) do
       stub_illiad_get_request(url: 'Transaction/UserRequests/testhelp', body: File.read('./spec/fixtures/illiad_requests.json'))
     end
     subject do
-      InterlibraryLoanRequests.for(uniqname: 'testhelp')
+      InterlibraryLoans.for(uniqname: 'testhelp')
     end
     context "#count" do
       it "returns total request item count" do
-        expect(subject.count).to eq(0)
+        expect(subject.count).to eq(1)
       end
     end
     context "#each" do
@@ -20,7 +20,7 @@ describe InterlibraryLoanRequests do
         subject.each do |item|
           items = items + item.class.name
         end
-        expect(items).to eq('')
+        expect(items).to eq('InterlibraryLoan')
       end
     end
   end
