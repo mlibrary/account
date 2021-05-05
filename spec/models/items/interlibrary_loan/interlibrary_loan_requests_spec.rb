712bb30ab088147d2c2ebf52bde5b0a3060a5749
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-05-05 14:38:48 -0400

    Adding request options for testing

--- spec/models/items/interlibrary_loan/interlibrary_loan_requests_spec.rb
+++ spec/models/items/interlibrary_loan/interlibrary_loan_requests_spec.rb
@@ -2,16 +2,20 @@ require 'spec_helper'
 require 'json'
 
 describe InterlibraryLoanRequests do
-  context "zero loans" do
+  context "one request" do
     before(:each) do
-      stub_illiad_get_request(url: 'Transaction/UserRequests/testhelp', body: File.read('./spec/fixtures/illiad_requests.json'))
+      requests = JSON.parse(File.read('./spec/fixtures/illiad_requests.json'))
+      requests.delete_at(0)
+      requests[2]["TransactionStatus"] = "Request Has Been Submitted to ILL"
+      requests[3]["TransactionStatus"] = "Awaiting Request Processing"
+      stub_illiad_get_request(url: 'Transaction/UserRequests/testhelp', body: requests.to_json)
     end
     subject do
       InterlibraryLoanRequests.for(uniqname: 'testhelp')
     end
     context "#count" do
       it "returns total request item count" do
-        expect(subject.count).to eq(0)
+        expect(subject.count).to eq(2)
       end
     end
     context "#each" do
@@ -20,16 +24,16 @@ describe InterlibraryLoanRequests do
         subject.each do |item|
           items = items + item.class.name
         end
-        expect(items).to eq('')
+        expect(items).to eq('InterlibraryLoanRequestInterlibraryLoanRequest')
       end
     end
     context "#empty?" do
       it "returns a boolean" do
-        expect(subject.empty?).to eq(true)
+        expect(subject.empty?).to eq(false)
       end
     end
     context "#item_text" do
-      it "returns 'item' if there is only one loan, or 'items' if there is not" do
+      it "returns 'item' if there is only one request, or 'items' if there is not" do
         expect(subject.item_text).to eq('items')
       end
     end
