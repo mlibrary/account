1a1e87e194c01c8fdc91898485e228ee92ff3bf2
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-05-19 13:30:55 -0400

    Checking history settings for session patron

--- models/session_patron.rb
+++ models/session_patron.rb
@@ -1,6 +1,6 @@
 class SessionPatron
   extend Forwardable
-  def_delegators :@alma_patron, :uniqname, :full_name, :can_book?
+  def_delegators :@alma_patron, :uniqname, :full_name, :can_book?, :keep_history
 
   def initialize(uniqname)
     @alma_patron = Patron.for(uniqname: uniqname)
@@ -9,7 +9,8 @@ class SessionPatron
     {
       uniqname: @alma_patron.uniqname,
       full_name: @alma_patron.full_name,
-      can_book: @alma_patron.can_book?
+      can_book: @alma_patron.can_book?,
+      keep_history: @alma_patron.keep_history
     }
   end
 end
