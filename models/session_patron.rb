5dd0ca8c248e81458979f5814fb8c10349d29e1b
Monique Rio <mrio@umich.edu>
2021-06-02 11:17:23 -0400

    better handles not in alma and not in circ history

--- models/session_patron.rb
+++ models/session_patron.rb
@@ -6,6 +6,7 @@ class SessionPatron
   def to_h
     {
       uniqname: @patron.uniqname,
+      in_alma: @patron.in_alma?,
       can_book: @patron.can_book?,
       confirmed_history_setting: @patron.confirmed_history_setting?
     }
