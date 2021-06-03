4ef08d29473c5e9fc18f419a4d56f8b91dc9a814
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-06-03 13:06:28 -0400

    Adding pagination to model and view

--- models/items/interlibrary_loan/interlibrary_loans.rb
+++ models/items/interlibrary_loan/interlibrary_loans.rb
@@ -1,19 +1,29 @@
 class InterlibraryLoans < Items
-  def initialize(parsed_response:)
+  attr_reader :pagination
+  def initialize(parsed_response:, pagination:)
     super
-    @items = parsed_response.filter_map { |item| InterlibraryLoan.new(item) if item["RequestType"] == "Loan" && item["TransactionStatus"] == "Checked Out to Customer" }
+    @items = parsed_response.filter_map { |item| InterlibraryLoan.new(item) if item["RequestType"] == "Loan" && item["TransactionStatus"] == "Checked Out to Customer" } || []
+    @pagination = pagination
   end
 
-  def self.for(uniqname:, client: ILLiadClient.new)
+  def self.for(uniqname:, offset: nil, limit: 15, client: ILLiadClient.new)
     url = "/Transaction/UserRequests/#{uniqname}" 
+    query = {}
+    query["offset"] = offset unless offset.nil?
+
+    limit.nil? ? query["limit"] = 15 : query["limit"] = limit
+    
     #TBDeleted 
     fake_data = JSON.parse(File.read('./spec/fixtures/illiad_requests.json'))
     fake_data[1]["RenewalsAllowed"] = true
     fake_data[1]["DueDate"] = "2022-06-02T00:00:00"
 
-    response = client.get(url)
+    response = client.get(url, query)
     if response.code == 200
-      InterlibraryLoans.new(parsed_response: fake_data) #should be response.parsed_response
+      pagination_params = { url: "/current-checkouts/interlibrary-loan", total: fake_data.count }
+      pagination_params[:limit] = limit unless limit.nil?
+      pagination_params[:current_offset] = offset unless offset.nil?
+      InterlibraryLoans.new(parsed_response: fake_data, pagination: PaginationDecorator.new(**pagination_params)) #should be response.parsed_response
     else
       #Error!
     end
