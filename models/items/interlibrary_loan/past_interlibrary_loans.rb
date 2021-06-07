510abf9657d25311e9d181420aca56819a1fc26f
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-06-07 11:13:22 -0400

    Adding pagination and filtering by API

--- models/items/interlibrary_loan/past_interlibrary_loans.rb
+++ models/items/interlibrary_loan/past_interlibrary_loans.rb
@@ -1,18 +1,25 @@
 class PastInterlibraryLoans < Items
-  def initialize(parsed_response:)
+  attr_reader :pagination
+  def initialize(parsed_response:, pagination:)
     super
-    actions = [
-      "Cancelled",
-      "Finished"
-    ]
-    @items = parsed_response.filter_map { |item| PastInterlibraryLoan.new(item) if actions.any?{|action| item["TransactionStatus"].include?(action)}}
+    @items = parsed_response
+    @pagination = pagination
   end
 
-  def self.for(uniqname:, client: ILLiadClient.new)
+  def self.for(uniqname:, offset: nil, limit: 15, client: ILLiadClient.new)
     url = "/Transaction/UserRequests/#{uniqname}" 
-    response = client.get(url)
+    query = {}
+    query["$filter"] = "TransactionStatus eq 'Request Finished' or startswith(TransactionStatus, 'Cancelled')"
+    query["$skip"] = offset unless offset.nil?
+
+    limit.nil? ? query["$top"] = 15 : query["$top"] = limit
+
+    response = client.get(url, query)
     if response.code == 200
-      PastInterlibraryLoans.new(parsed_response: response.parsed_response)
+      pagination_params = { url: "/past-activity/interlibrary-loan", total: response.parsed_response.count }
+      pagination_params[:limit] = limit unless limit.nil?
+      pagination_params[:current_offset] = offset unless offset.nil?
+      PastInterlibraryLoans.new(parsed_response: response.parsed_response, pagination: PaginationDecorator.new(**pagination_params))
     else
       #Error!
     end
