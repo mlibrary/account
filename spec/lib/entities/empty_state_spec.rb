a199234ac6d4454ec19bf0f9a5a631af1f98b574
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-10-14 10:02:39 -0400

    Readding overwritten changes

--- spec/lib/entities/empty_state_spec.rb
+++ spec/lib/entities/empty_state_spec.rb
@@ -1,6 +1,8 @@
 describe Entities::Page::EmptyState do
   let(:default_heading){'You have no items.'}
   let(:default_message){ "See <a href=\"https://www.lib.umich.edu/find-borrow-request\">what you can borrow from the library<\/a>."}
+  let(:default_heading_tag){"h3"}
+  let(:parentless_heading_tag){ "h2"}
                       
   before(:each) do
     @empty_state = { "heading" => 'This is a heading', "message" => 'This is a message.' }
@@ -10,23 +12,42 @@ describe Entities::Page::EmptyState do
     described_class.new(@empty_state, @parent_empty_state)
   end
   it "handles given empty state" do
-    expect(subject.heading) == @empty_state["heading"]
-    expect(subject.message) == @empty_state["message"]
+    expect(subject.heading).to eq(@empty_state["heading"])
+    expect(subject.message).to eq(@empty_state["message"])
+    expect(subject.heading_tag).to eq(parentless_heading_tag)
   end
-  it "handles  nil both" do
+  it "handles nil both" do
     @empty_state = nil
-    expect(subject.heading) == default_heading
-    expect(subject.message) == default_message
+    expect(subject.heading).to eq(default_heading)
+    expect(subject.message).to eq(default_message)
+    expect(subject.heading_tag).to eq(parentless_heading_tag)
   end
   it "if nil, uses parent if available" do
     @parent_empty_state = described_class.new({"heading" => "Parent Heading", "message" => "Parent Message"}, nil)
     @empty_state = nil
     expect(subject.heading).to eq("Parent Heading")
     expect(subject.message).to eq("Parent Message")
+    expect(subject.heading_tag).to eq(default_heading_tag)
   end
   it "uses default if one is missing" do
     @empty_state.delete("message")
-    expect(subject.heading) == @empty_state["heading"]
-    expect(subject.message) == default_message
+    expect(subject.heading).to eq(@empty_state["heading"])
+    expect(subject.message).to eq(default_message)
+    expect(subject.heading_tag).to eq(parentless_heading_tag)
+  end
+  it "uses default if two are missing" do
+    @empty_state.delete("heading")
+    @empty_state.delete("message")
+    expect(subject.heading).to eq(default_heading)
+    expect(subject.message).to eq(default_message)
+    expect(subject.heading_tag).to eq(parentless_heading_tag)
+  end
+  it "if no parent, `heading_tag` changes to `h2`" do
+    @parent_empty_state = nil
+    expect(subject.heading_tag).to eq(parentless_heading_tag)
+  end
+  it "if parent exists, `heading_tag` changes to `h3`" do
+    @parent_empty_state = {}
+    expect(subject.heading_tag).to eq(default_heading_tag)
   end
 end
