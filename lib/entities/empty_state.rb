1754a23de2255b3ac64f652dec7e5b86aad4f199
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-10-14 08:45:23 -0400

    Removing [DAT-60] Headings changes

--- lib/entities/empty_state.rb
+++ lib/entities/empty_state.rb
@@ -1,9 +1,8 @@
 class Entities::Page::EmptyState
-  attr_reader :heading, :message, :heading_tag
+  attr_reader :heading, :message
   def initialize(empty_state, parent_empty_state=nil)
     @heading = "You have no items."
     @message = "See <a href=\"https://www.lib.umich.edu/find-borrow-request\">what you can borrow from the library<\/a>."
-    @heading_tag = "h3"
     
     if !empty_state.nil?
       @heading = empty_state["heading"] || @heading
@@ -11,10 +10,6 @@ class Entities::Page::EmptyState
     elsif !parent_empty_state.nil?
       @heading = parent_empty_state.heading || @heading
       @message = parent_empty_state.message || @message
-    end
-
-    if parent_empty_state.nil?
-      @heading_tag = "h2"
     end
   end
 end
