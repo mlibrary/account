03a84c2cf2572083ebb10da497b1d65ab4a3ff26
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-10-15 14:42:48 -0400

    Converting heading_tag into a method, updating tests

--- lib/entities/empty_state.rb
+++ lib/entities/empty_state.rb
@@ -1,20 +1,24 @@
 class Entities::Page::EmptyState
-  attr_reader :heading, :message, :heading_tag
+  attr_reader :heading, :message
   def initialize(empty_state, parent_empty_state=nil)
     @heading = "You have no items."
     @message = "See <a href=\"https://www.lib.umich.edu/find-borrow-request\">what you can borrow from the library<\/a>."
-    @heading_tag = "h3"
+    @parent_empty_state = parent_empty_state
     
     if !empty_state.nil?
       @heading = empty_state["heading"] || @heading
       @message = empty_state["message"] || @message
     elsif !parent_empty_state.nil?
-      @heading = parent_empty_state.heading || @heading
-      @message = parent_empty_state.message || @message
+      @heading = @parent_empty_state.heading || @heading
+      @message = @parent_empty_state.message || @message
     end
+  end
 
-    if parent_empty_state.nil?
-      @heading_tag = "h2"
+  def heading_tag
+    if @parent_empty_state.nil?
+      "h2"
+    else
+      "h3"
     end
   end
 end
--- spec/lib/entities/empty_state_spec.rb
+++ spec/lib/entities/empty_state_spec.rb
@@ -1,8 +1,6 @@
 describe Entities::Page::EmptyState do
   let(:default_heading){'You have no items.'}
   let(:default_message){ "See <a href=\"https://www.lib.umich.edu/find-borrow-request\">what you can borrow from the library<\/a>."}
-  let(:default_heading_tag){"h3"}
-  let(:parentless_heading_tag){ "h2"}
                       
   before(:each) do
     @empty_state = { "heading" => 'This is a heading', "message" => 'This is a message.' }
@@ -14,40 +12,30 @@ describe Entities::Page::EmptyState do
   it "handles given empty state" do
     expect(subject.heading).to eq(@empty_state["heading"])
     expect(subject.message).to eq(@empty_state["message"])
-    expect(subject.heading_tag).to eq(parentless_heading_tag)
   end
   it "handles nil both" do
     @empty_state = nil
     expect(subject.heading).to eq(default_heading)
     expect(subject.message).to eq(default_message)
-    expect(subject.heading_tag).to eq(parentless_heading_tag)
   end
   it "if nil, uses parent if available" do
     @parent_empty_state = described_class.new({"heading" => "Parent Heading", "message" => "Parent Message"}, nil)
     @empty_state = nil
     expect(subject.heading).to eq("Parent Heading")
     expect(subject.message).to eq("Parent Message")
-    expect(subject.heading_tag).to eq(default_heading_tag)
   end
   it "uses default if one is missing" do
     @empty_state.delete("message")
     expect(subject.heading).to eq(@empty_state["heading"])
     expect(subject.message).to eq(default_message)
-    expect(subject.heading_tag).to eq(parentless_heading_tag)
   end
-  it "uses default if two are missing" do
-    @empty_state.delete("heading")
-    @empty_state.delete("message")
-    expect(subject.heading).to eq(default_heading)
-    expect(subject.message).to eq(default_message)
-    expect(subject.heading_tag).to eq(parentless_heading_tag)
-  end
-  it "if no parent, `heading_tag` changes to `h2`" do
-    @parent_empty_state = nil
-    expect(subject.heading_tag).to eq(parentless_heading_tag)
-  end
-  it "if parent exists, `heading_tag` changes to `h3`" do
-    @parent_empty_state = {}
-    expect(subject.heading_tag).to eq(default_heading_tag)
-  end
+  context "#heading_tag" do
+    it "returns `h2` if parent unavailable" do
+      expect(subject.heading_tag).to eq("h2")
+    end
+    it "returns `h3` if parent available" do
+      @parent_empty_state = {}
+      expect(subject.heading_tag).to eq("h3")
+    end
+  end 
 end
