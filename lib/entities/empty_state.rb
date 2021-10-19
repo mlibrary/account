56047b1d1bfd6e4eaa72ee6747ccabd5a2db0ec4
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-10-19 15:46:34 -0400

    Turning heading and message into methods

--- lib/entities/empty_state.rb
+++ lib/entities/empty_state.rb
@@ -1,16 +1,26 @@
 class Entities::Page::EmptyState
-  attr_reader :heading, :message
   def initialize(empty_state, parent_empty_state=nil)
-    @heading = "You have no items."
-    @message = "See <a href=\"https://www.lib.umich.edu/find-borrow-request\">what you can borrow from the library<\/a>."
+    @empty_state = empty_state
     @parent_empty_state = parent_empty_state
-    
-    if !empty_state.nil?
-      @heading = empty_state["heading"] || @heading
-      @message = empty_state["message"] || @message
-    elsif !parent_empty_state.nil?
-      @heading = @parent_empty_state.heading || @heading
-      @message = @parent_empty_state.message || @message
+  end
+
+  def heading
+    if @empty_state&.key?("heading")
+      @empty_state["heading"]
+    elsif @parent_empty_state&.heading
+      @parent_empty_state.heading
+    else
+      "You have no items."
+    end
+  end
+
+  def message
+    if @empty_state&.key?("message")
+      @empty_state["message"]
+    elsif @parent_empty_state&.message
+      @parent_empty_state.message
+    else
+      "See <a href=\"https://www.lib.umich.edu/find-borrow-request\">what you can borrow from the library<\/a>."
     end
   end
 
