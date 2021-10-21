54504bc2ef8ba7793f2b3e3a30a7d616352f68f9
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-10-21 15:28:22 -0400

    Cleaning up model and specs - Removing unused methods - Updating specs that look for the methods

--- models/response/renew_response_presenter.rb
+++ models/response/renew_response_presenter.rb
@@ -1,34 +1,14 @@
 class RenewResponsePresenter
-  attr_reader :renewed, :not_renewed
-  def initialize(renewed:, not_renewed:)
+  attr_reader :renewed
+  def initialize(renewed:)
     @renewed = renewed
-    @not_renewed = not_renewed
-  end
-  def renewed?
-    @renewed > 0
-  end
-  def not_renewed?
-    @not_renewed > 0
   end
   def renewed_text
-    "#{@renewed} #{item(@renewed)} #{verb(@renewed)} successfully renewed."
-  end
-  def not_renewed_text
-    "#{@not_renewed} #{item(@not_renewed)} #{verb(@not_renewed)} unable to be renewed for one of the following reasons:"
-  end
-  def unrenewable_reasons
-    [
-      "Item has exceeded the number of renews allowed",
-      "Item is for building-use only",
-      "Item has been reported as lost",
-    ]
-  end
-  def not_renewed_continued_text
-    [
-      "Review the <span class='strong'>Actions</span> column of your checkouts to see which items did not renew.",
-      "If you do not see any changes after attempting to renew items, there may have been an unexpected network error. Please refresh the page and try again.",
-      "If you have questions or need help, please contact the <a href=\"https://lib.umich.edu/locations-and-hours/hatcher-library/hatcher-north-information-services-desk\">Hatcher North Information Services Desk</a>.",
-    ]
+    if @renewed == 0
+      "Eligible #{item(@renewed)} have been renewed."
+    else
+      "#{@renewed} #{item(@renewed)} #{verb(@renewed)} successfully renewed."
+    end
   end
   private
   def verb(count)
