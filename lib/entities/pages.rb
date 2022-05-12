23d4560ef3c839fa03a0605cb2928843e08e1b67
Erin E. Sullivan <sulliv.erin@gmail.com>
2022-05-12 13:20:39 -0400

    Removing page from sidebar navigation

--- lib/entities/pages.rb
+++ lib/entities/pages.rb
@@ -30,7 +30,7 @@ class Entities::Page
     @parent = parent
     @children = page.dig("children")&.map { |p| Entities::Page.new(p, self) }
   end
-  ["title", "description", "icon_name", "color", "dropdown"].each do |name|
+  ["title", "description", "icon_name", "color", "dropdown", "sidebar"].each do |name|
     define_method(name) do
       @page[name]
     end
