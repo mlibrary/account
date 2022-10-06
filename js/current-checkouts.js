11db499be1616a7d7b35a829cdeb2f39ca3bd203
Erin E. Sullivan <sulliv.erin@gmail.com>
2022-10-06 09:42:25 -0400

    Rewriting JavaScript to support Renew all eligible - Progress bar has been simplified

--- js/current-checkouts.js
+++ js/current-checkouts.js
@@ -1,113 +1,144 @@
 /**
- * Renew all eligible items
+ * Renew individual items
  */
 
+const errorMessage = (loanID) => {
+  let message = 'Could not retrieve loans.';
+  if (loanID) {
+    message = `Could not renew loan id ${loanID}.`;
+  }
+  throw new Error(message);
+};
+
+const renewingLoan = async (loanID) => {
+  try {
+    const response = await fetch(`/renew-loan?loan_id=${loanID}`, {
+      method: 'POST'
+    });
+    if (await response.status !== 200) {
+      errorMessage(loanID);
+    }
+    return await response.json();
+  } catch (error) {}
+};
+
+const renewingItem = async (loanID) => {
+  try {
+    const renewLoan = await renewingLoan(loanID);
+    if (!renewLoan) {
+      errorMessage(loanID);
+    }
+    // Update due date of loans if on current page
+    const loanDueDate = document.querySelector(`[data-loan-due-date="${loanID}"]`);
+    if (renewLoan.due_date && loanDueDate) {
+      loanDueDate.innerHTML = renewLoan.due_date;
+    }
+    return renewLoan;
+  } catch (error) {}
+};
+
+const renewItemButtons = document.querySelectorAll('[data-js-renew]');
+
+renewItemButtons.forEach((renewItemButton) => {
+  renewItemButton.addEventListener('click', async (event) => {
+    event.target.innerHTML = 'Processing...';
+    const loanID = event.target.dataset.jsRenew;
+    try {
+      const renewItem = await renewingItem(loanID);
+      if (!renewItem) {
+        errorMessage(loanID);
+      }
+      event.target.innerHTML = 'Renewed!';
+      event.target.disabled = true;
+      return renewItem;
+    } catch (error) {
+      event.target.innerHTML = 'Error!';
+      console.error(error);
+    }
+  });
+  renewItemButton.disabled = false;
+});
+
+/**
+ * Renew eligible items
+ */
+
+const getAllItems = async () => {
+  try {
+    const response = await fetch('/current-checkouts/u-m-library/all');
+    if (await response.status !== 200) {
+      errorMessage();
+    }
+    return await response.json();
+  } catch (error) {}
+};
+
+const renewAllEligibleButton = document.querySelector('[data-js-renew-all]');
 // Progress bar elements
 const progressBar = document.querySelector('.progress-bar');
 const progress = progressBar.querySelector('progress');
-const progressValue = progress.getAttribute('value');
-const progressMax = progress.getAttribute('max');
 const progressDescription = progressBar.querySelector('#progress-description');
 const progressDescriptionRenewed = progressDescription.querySelector('.renewed-loans');
 const progressDescriptionTotal = progressDescription.querySelector('.total-loans');
 
-// Fetch all current items
-async function getAllItems () {
+const getEligibleItems = async () => {
   try {
-    const response = await fetch('/current-checkouts/u-m-library/all');
-    return response.json();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-// Filter items by those that are eligible for renewal
-async function getEligibleItems () {
-  const allItems = await getAllItems();
-  const filteredRenewableItems = allItems.filter((item) => {
-    return item.renewable;
-  }).map((item) => {
-    return item.loan_id;
-  });
-  return filteredRenewableItems;
-}
-
-// Renew eligible item
-function renewEligibleItem (loanID, step, lastItem) {
-  fetch(`/renew-loan?loan_id=${loanID}`, {
-    method: 'POST'
-  }).then((response) => {
-    return response.json();
-  }).then((data) => {
-    // Convert current progress value to an integer, and add the step
-    const newValue = parseInt(progress.getAttribute('value')) + step;
-    // Set new value to maximum amount if ends up being larger
-    progress.setAttribute('value', newValue > progressMax ? progressMax : newValue);
-    // Update renewed count
-    progressDescriptionRenewed.innerText++;
-    // Update due date of loans if on current page
-    if (data.due_date) {
-      document.querySelector(`[data-loan-due-date="${loanID}"]`).innerHTML = data.due_date;
+    const allItems = await getAllItems();
+    if (!allItems) {
+      errorMessage();
     }
-    return lastItem;
-  }).then((lastItem) => {
-    if (lastItem) {
-      // Hide the progress bar
-      progressBar.style.display = 'none';
-      // Show message
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
-}
-
-// Get the `Renew all eligible` button
-const renewAllEligibleButton = document.querySelector('[data-js-renew-all]');
-// Wait until eligible items have been retrieved
-getEligibleItems().then((items) => {
-  // Update loan count on button and progress details
-  renewAllEligibleButton.dataset.jsRenewAll = progressDescriptionTotal.innerText = items.length;
-  // Undisable button if there are loans that can be renewed
-  renewAllEligibleButton.disabled = items.length === 0;
-  return items;
-}).then((items) => {
-  renewAllEligibleButton.addEventListener('click', async function (event) {
-    progress.setAttribute('value', progressValue);
-    const progressStep = Math.ceil(progressMax / items.length);
+    // Filter items by those that are eligible for renewal, and grab the loan ID
+    const filteredEligibleItems = allItems
+      .filter((item) => {
+        return item.renewable;
+      }).map((item) => {
+        return item.loan_id;
+      });
+    const totalEligibleItems = filteredEligibleItems.length;
+    // Update loan count on button and progress details
+    renewAllEligibleButton.dataset.jsRenewAll = progressDescriptionTotal.innerText = totalEligibleItems;
+    progress.setAttribute('max', totalEligibleItems);
+    // Undisable button if there are loans that can be renewed
+    renewAllEligibleButton.disabled = totalEligibleItems === 0;
+    // Reset progress
+    progress.setAttribute('value', 0);
     progressDescriptionRenewed.innerText = 0;
-    progressBar.style.display = 'block';
-    Promise.allSettled(items.map((loanID, index) => {
-      return renewEligibleItem(loanID, progressStep, items.length === index + 1);
-    }));
-  });
-});
+    return filteredEligibleItems;
+  } catch (error) {}
+};
 
-/**
- * Renew single item
- */
-const renewItems = document.querySelectorAll('[data-js-renew]');
-renewItems.forEach((renewItem) => {
-  renewItem.addEventListener('click', (event) => {
-    event.target.innerHTML = 'Processing...';
-    const loanID = event.target.dataset.jsRenew;
-    fetch(`/renew-loan?loan_id=${loanID}`, {
-      method: 'POST'
-    }).then((response) => {
-      if (response.status === 200) {
-        return response.json();
-      }
-      event.target.innerHTML = 'Error!';
-      throw new Error(`Could not renew loan id ${loanID}.`);
-    }).then((data) => {
-      if (data.due_date) {
-        document.querySelector(`[data-loan-due-date="${loanID}"]`).innerHTML = data.due_date;
-        event.target.innerHTML = 'Renewed!';
-        event.target.disabled = true;
-      }
-      return data;
-    }).catch((error) => {
-      console.error(error);
-    });
-  });
-  renewItem.disabled = false;
+(async function () {
+  await getEligibleItems();
+}());
+
+renewAllEligibleButton.addEventListener('click', async function (event) {
+  // Start loading animation
+  event.target.classList.add('loading');
+  // Show progress bar
+  progressBar.style.display = 'block';
+  progress.setAttribute('aria-busy', true);
+  try {
+    const eligibleItems = await getEligibleItems();
+    if (!eligibleItems) {
+      errorMessage();
+    }
+    return await Promise.allSettled(eligibleItems.map((eligibleItem, index) => {
+      // Update renewal progress
+      const currentCount = index + 1;
+      progressDescriptionRenewed.innerText = progress.value = currentCount;
+      return renewingItem(eligibleItem);
+    }));
+    // Assign message
+  } catch (error) {
+    // Assign message
+  } finally {
+    // Stop loading animation
+    event.target.classList.remove('loading');
+    // Hide progress bar
+    progressBar.style.display = 'none';
+    progress.setAttribute('aria-busy', false);
+    // Show message
+    // Fetch items again
+    await getEligibleItems();
+  }
 });
